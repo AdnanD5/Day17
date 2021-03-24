524ee09c2400675324fe872b6e17a890b2f768d6
AdnanD5 <adodervisevic55@hotmail.com>
2021-03-24 22:43:41 +0100

    Dodavanje

---
+++ ex08/src/index.js
@@ -0,0 +1,36 @@
+//Lodash
+const _= require('lodash');
+//Only change code below this line
+const users = {
+    {firstName = "John",
+     lastName = "Doe",
+     age = 24,
+     gender = "male",};
+
+     {firstName = "Jane",
+     lastName = "Doe",
+     age = 5,
+     gender = "female",};
+
+     {firstName = "Jim",
+     lastName = "Carrey",
+     age = 54,
+     gender = "male",};
+
+     {firstName = "Kate",
+     lastName = "Winslet",
+     age = 54,
+     gender = "female",}
+}
+function getUsers(){
+    var output = [];
+    for(var i=0, i<output.length, i++){
+        console.log(output[i])
+    }
+    return output
+}
+function findUser(lastName, gender){
+    try(){
+        var
+    }
+}
